36fd93ae0bfc2873da7b9634a72fadd81112e6f3
kyulipark <kyuli94p@gmail.com>
2024-08-18 18:48:12 +0900

    걷기반 실습1

---
+++ day1-1.js
@@ -0,0 +1,30 @@
+//1번
+// const greeting = "Hello";
+// const name = "World";
+
+// console.log(greeting + name);
+
+//2번
+// const lion = {
+//   name: "simba",
+//   age: 3,
+//   main: true,
+//   대사: function () {
+//     return "하쿠나마타타";
+//   },
+// };
+
+// console.log(lion.대사());
+
+//3번
+// const sentence = "Learning JavaScript is fun!";
+// const newSentence = sentence.replace("JavaScript", "Coding");
+// console.log(newSentence);
+
+//4번
+// function incrementAge(lion) {
+//   lion.age = lion.age + 1;
+//   return lion;
+// }
+
+// console.log(incrementAge(lion));
